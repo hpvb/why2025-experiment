be97595251b554c8959c3f81308bf721eb8c6420
HP van Braam <hp@tmm.cx>
2025-06-22 15:29:48 +0200

    Change bootloader address

--- linux/flash.sh
+++ linux/flash.sh
@@ -7,7 +7,7 @@ CROSS_COMPILE=riscv64-linux-gnu- make Image ARCH=riscv -j32
 gen_esp32part.py partition-table.csv partition-table.bin
 esptool.py --chip esp32p4 --port /dev/ttyUSB0 --baud 921600 \
   write_flash \
-  0x0 linux_loader/build/bootloader/bootloader.bin \
+  0x1000 linux_loader/build/bootloader/bootloader.bin \
   0x8000 linux_loader/build/partition_table/partition-table.bin \
   0x10000 linux_loader/build/linux_loader.bin \
   0x210000 arch/riscv/boot/Image
